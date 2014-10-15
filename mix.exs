b6038d4e0b82afbc0a6b5ab1cd8bd69ecd237421
Martin Schürrer <martin@schuerrer.org>
2014-10-15 16:51:04 +0200

    Add sidejob dependency

--- mix.exs
+++ mix.exs
@@ -8,23 +8,13 @@ defmodule SideTask.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :sidejob]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      {:sidejob, github: "basho/sidejob", tag: "2.0.0"},
+    ]
   end
 end
