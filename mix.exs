be76c522ad7fbe0e7aeb5b69c90385fd64d382f0
Martin Schürrer <martin@schuerrer.org>
2014-10-15 21:07:11 +0200

    Make hex happy

--- mix.exs
+++ mix.exs
@@ -6,12 +6,32 @@ defmodule SideTask.Mixfile do
      version: "0.1.0",
      elixir: "~> 1.0",
      source_url: "https://github.com/MSch/sidetask",
-     licenses: ["Apache 2.0"],
+     description: description,
+     package: package,
      deps: deps]
   end
 
   def application do
     [applications: [:logger, :sidejob]]
+  end
+
+  defp description do
+    """
+    SideTask is an alternative to Elixir's Task.Supervisor that uses Basho's sidejob library for
+    better parallelism and to support capacity limiting of Tasks.
+
+    SideTask provides an API similar to Task.Supervisor, with the addition that all calls that start
+    a new task require a sidejob resource as argument and can return `{:error, :overload}`.
+
+    Convenience functions for adding and deleting sidejob resources are provided.
+    """
+  end
+
+  defp package do
+    [contributors: ["Martin Schurrer"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/MSch/sidetask"},
+     files: ["lib", "mix.exs", "README.md"]]
   end
 
   defp deps do
