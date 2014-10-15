2486a5e9f82a8c639f36a60d5e7916375fc5090a
Martin Schürrer <martin@schuerrer.org>
2014-10-15 20:30:23 +0200

    Sync sidetask source

--- lib/sidetask.ex
+++ lib/sidetask.ex
@@ -1,2 +1,104 @@
 defmodule SideTask do
+  @moduledoc ~S"""
+  An alternative to Elixir's `Task.Supervisor` that uses Basho's `sidejob` library for better
+  parallelism and to support capacity limiting.
+
+  Elixir's `Task.Supervisor` is implemented as a single `:simple_one_for_one` supervisor with the
+  individual `Task`s as children. This means starting a new task has to go through this single
+  supervisor. Furthermore there is no limit to the number of works that can be running at the same
+  time.
+
+  Basho's `sidejob` library spawns multiple supervisors (one for each scheduler by default) and
+  uses ETS tables to keep track of the number of workers, thereby making it possible to start
+  workers in parallel while also putting an upper bound on the number of running workers.
+
+  This module provides an API similar to `Task.Supervisor`, with the addition that all calls that
+  starting a new task require a sidejob resource as argument and can return `{:error, :overload}`.
+
+  The `add_resource/2` and `delete_resource/1` functions are provided as convenience.
+
+  Name Registration
+  A sidejob resource registers multiple local names. Read more about it in the sidejob docs.
+  """
+
+  @typedoc "The sidejob resource"
+  @type resource :: atom
+
+  @doc """
+  Create a new sidejob resource that enforces the requested usage limit.
+
+  See `:sidejob.new_resource/4` for details.
+  """
+  @spec add_resource(resource, non_neg_integer) :: :ok
+  def add_resource(name, limit) when is_atom(name) and is_integer(limit) do
+    {:ok, _} = :sidejob.new_resource(name, :sidejob_supervisor, limit)
+    :ok
+  end
+
+  @doc """
+  Deletes an existing sidejob resource.
+  """
+  @spec delete_resource(resource) :: :ok | {:error, error}
+        when error: :not_found | :running | :restarting
+  def delete_resource(name) when is_atom(name) do
+    case Supervisor.terminate_child(:sidejob_sup, name) do
+      :ok -> Supervisor.delete_child(:sidejob_sup, name)
+      error -> error
+    end
+  end
+
+  @doc """
+  Starts a task that can be awaited on as child of the given `sidejob_resource`
+  """
+  @spec async(resource, fun) :: {:ok, Task.t} | {:error, :overload}
+  def async(sidejob_resource, fun) do
+    async(sidejob_resource, :erlang, :apply, [fun, []])
+  end
+
+  @doc """
+  Starts a task that can be awaited on as child of the given `sidejob_resource`
+  """
+  @spec async(resource, module, atom, [term]) :: {:ok, Task.t} | {:error, :overload}
+  def async(sidejob_resource, module, fun, args) do
+    args = [self, get_info(self), {module, fun, args}]
+    case :sidejob_supervisor.start_child(sidejob_resource, Task.Supervised, :start_link, args) do
+      {:ok, pid} ->
+        ref = Process.monitor(pid)
+        send pid, {self(), ref}
+        {:ok, %Task{pid: pid, ref: ref}}
+      {:error, :overload} -> {:error, :overload}
+    end
+  end
+
+  @doc """
+  Starts a task as child of the given `sidejob_resource`.
+
+  Note that the spawned process is not linked to the caller, but only to the sidejob resource's
+  supervisor.
+  """
+  @spec start_child(resource, fun) :: {:ok, pid} | {:error, :overload}
+  def start_child(sidejob_resource, fun) do
+    start_child(sidejob_resource, :erlang, :apply, [fun, []])
+  end
+
+  @doc """
+  Starts a task as child of the given `sidejob_resource`.
+
+  Note that the spawned process is not linked to the caller, but only to the sidejob resource's
+  supervisor.
+  """
+  @spec start_child(resource, module, atom, [term]) :: {:ok, pid} | {:error, :overload}
+  def start_child(sidejob_resource, module, fun, args) do
+    args = [get_info(self), {module, fun, args}]
+    :sidejob_supervisor.start_child(sidejob_resource, Task.Supervised, :start_link, args)
+  end
+
+  # from Task.Supervisor.get_info
+  defp get_info(self) do
+    {node(),
+     case Process.info(self, :registered_name) do
+       {:registered_name, []} -> self()
+       {:registered_name, name} -> name
+     end}
+  end
 end
--- test/sidetask_test.exs
+++ test/sidetask_test.exs
@@ -1,7 +1,133 @@
 defmodule SideTaskTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup do
+    SideTask.add_resource(:test, 3)
+    on_exit fn -> SideTask.delete_resource(:test) end
+    {:ok, resource: :test}
+  end
+
+  setup do
+    Logger.remove_backend(:console)
+    on_exit fn -> Logger.add_backend(:console, flush: true) end
+    :ok
+  end
+
+  def wait_and_send(caller, atom) do
+    send caller, :ready
+    receive do: (true -> true)
+    send caller, atom
+  end
+
+  test "async can trip sidejob's overload protection", config do
+    # test that sidejob limits worker count and returns `{:error, :overload}` when overloaded.
+    # sidejob's actual limit is number of scheduler * configured limit and is dependent on the
+    # tick time of the individual sidejob workers, so we can just check that we get an overload
+    # error some of the time if we request "too many" workers.
+    acquired_workers = for _ <- 1..(:erlang.system_info(:schedulers)*4) do
+      SideTask.async(config[:resource], fn -> :timer.sleep(100) end)
+    end
+    assert Enum.member?(acquired_workers, {:error, :overload})
+  end
+
+  test "start_child can trip sidejob's overload protection", config do
+    # c.f. corresponding test for async
+    acquired_workers = for _ <- 1..(:erlang.system_info(:schedulers)*4) do
+      SideTask.async(config[:resource], fn -> :timer.sleep(100) end)
+    end
+    assert Enum.member?(acquired_workers, {:error, :overload})
+  end
+
+
+  ## Adapted tests from Task.SupervisorTest
+
+  test "async/1", config do
+    parent = self()
+    fun = fn -> wait_and_send(parent, :done) end
+    {:ok, task} = SideTask.async(config[:resource], fun)
+
+    # Assert the struct
+    assert task.__struct__ == Task
+    assert is_pid task.pid
+    assert is_reference task.ref
+
+    # Assert the link
+    {:links, links} = Process.info(self, :links)
+    assert task.pid in links
+
+    receive do: (:ready -> :ok)
+
+    # Assert the initial call
+    {:name, fun_name} = :erlang.fun_info(fun, :name)
+    assert {__MODULE__, fun_name, 0} === :proc_lib.translate_initial_call(task.pid)
+
+    # Run the task
+    send task.pid, true
+
+    # Assert response and monitoring messages
+    ref = task.ref
+    assert_receive {^ref, :done}
+    assert_receive {:DOWN, ^ref, _, _, :normal}
+  end
+
+  test "async/3", config do
+    {:ok, task} = SideTask.async(config[:resource], __MODULE__, :wait_and_send, [self(), :done])
+
+    receive do: (:ready -> :ok)
+    assert {__MODULE__, :wait_and_send, 2} === :proc_lib.translate_initial_call(task.pid)
+
+    send task.pid, true
+    assert task.__struct__ == Task
+    assert Task.await(task) == :done
+  end
+
+  test "start_child/1", config do
+    parent = self()
+    fun = fn -> wait_and_send(parent, :done) end
+    {:ok, pid} = SideTask.start_child(config[:resource], fun)
+
+    {:links, links} = Process.info(self, :links)
+    refute pid in links
+
+    receive do: (:ready -> :ok)
+    {:name, fun_name} = :erlang.fun_info(fun, :name)
+    assert {__MODULE__, fun_name, 0} === :proc_lib.translate_initial_call(pid)
+
+    send pid, true
+    assert_receive :done
+  end
+
+  test "start_child/3", config do
+    {:ok, pid} = SideTask.start_child(config[:resource], __MODULE__, :wait_and_send, [self(), :done])
+
+    {:links, links} = Process.info(self, :links)
+    refute pid in links
+
+    receive do: (:ready -> :ok)
+    assert {__MODULE__, :wait_and_send, 2} === :proc_lib.translate_initial_call(pid)
+
+    send pid, true
+    assert_receive :done
+  end
+
+  test "await/1 exits on task throw", config do
+    Process.flag(:trap_exit, true)
+    {:ok, task} = SideTask.async(config[:resource], fn -> throw :unknown end)
+    assert {{{:nocatch, :unknown}, _}, {Task, :await, [^task, 5000]}} =
+           catch_exit(Task.await(task))
+  end
+
+  test "await/1 exits on task error", config do
+    Process.flag(:trap_exit, true)
+    {:ok, task} = SideTask.async(config[:resource], fn -> raise "oops" end)
+    assert {{%RuntimeError{}, _}, {Task, :await, [^task, 5000]}} =
+           catch_exit(Task.await(task))
+  end
+
+  test "await/1 exits on task exit", config do
+    Process.flag(:trap_exit, true)
+    {:ok, task} = SideTask.async(config[:resource], fn -> exit :unknown end)
+    assert {:unknown, {Task, :await, [^task, 5000]}} =
+           catch_exit(Task.await(task))
   end
 end
